a9c1d3cd8bcae9cf51c984fafb8969e470505198
AliReza <khazaeinezhadalireza@gmail.com>
2022-03-20 09:45:48 +0330

    Initial

---
+++ KhN.FarazSMS/KhN.FarazSMS.Cons/Program.cs
@@ -0,0 +1,14 @@
+﻿
+
+using System;
+
+namespace KhN.FarazSMS.Cons
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ReadKey();
+        }
+    }
+}
